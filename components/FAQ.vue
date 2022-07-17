17b4db2782be68e55bbfd8592b633995937f82fd
Egovar <egovar371@gmail.com>
2022-07-17 19:18:15 +0300

    FAQ

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -1,13 +1,16 @@
 <template>
   <section class="faq">
     <h2 class="faq__title">Частые вопросы /</h2>
+    <Accordion :items="faq" title-field="question" content-field="answer" />
   </section>
 </template>
 
 <script>
 import faq from '@/const/faq';
+import Accordion from '@/components/core/organisms/Accordion';
 export default {
   name: 'FAQ',
+  components: { Accordion },
   data() {
     return {
       faq,
@@ -19,6 +22,7 @@ export default {
 <style scoped lang="scss">
 .faq {
   margin-top: 12.5rem;
+  margin-bottom: 5.625rem;
   &__title {
     @include section-title;
   }
---
+++ components/core/organisms/Accordion.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="accordion">
+    <PrivateAccordionItem
+      v-for="item in items"
+      :key="item[titleField]"
+      :title="item[titleField]"
+      :content="item[contentField]"
+      class="accordion__item"
+    />
+  </div>
+</template>
+
+<script>
+import PrivateAccordionItem from '@/components/core/molecules/PrivateAccordionItem';
+export default {
+  name: 'Accordion',
+  components: { PrivateAccordionItem },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    titleField: {
+      type: String,
+      default: 'title',
+    },
+    contentField: {
+      type: String,
+      default: 'content',
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.accordion {
+  border-top: 1px solid $main-text-color;
+  border-bottom: 1px solid $main-text-color;
+  &__item {
+    &:not(:first-child) {
+      border-top: 1px solid $main-text-color;
+    }
+  }
+}
+</style>
